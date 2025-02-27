d6d3947fb2fea4121e59abd17aa35b0d8380f8d0
Dhouha Lamine <dhouha.lamin@gmail.com>
2025-02-27 15:37:23 +0100

    Project-setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to the Recipe Finder App</h1>
+    </div>
+  );
+}
+
+export default App;
